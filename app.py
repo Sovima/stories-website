05df79d2a3bac034dd722bd188c6c94dd81b7b70
Sovima <malashchenko1329@gmail.com>
2022-12-08 22:10:05 -0500

    Add routs to other pages

--- app.py
+++ app.py
@@ -5,3 +5,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/stories")
+def stories():
+    return render_template("stories.html")
+
+
+@app.route("/puss-in-boots")
+def puss_in_boots():
+    return render_template("puss-in-boots.html")
