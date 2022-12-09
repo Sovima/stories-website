7c7743241c6d94d12fb0e4e82d6f7120763531e8
Sovima <malashchenko1329@gmail.com>
2022-12-09 12:38:52 -0500

    Add routs to css files

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", css_link="/static/css/home.css")
+    return render_template("index.html", css_link = "/static/css/home.css")
 
 
 @app.route("/stories")
