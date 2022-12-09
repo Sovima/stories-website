7da9a25ae15f8207ac83ca707d4d5162025b07a4
Sovima <malashchenko1329@gmail.com>
2022-12-09 12:28:34 -0500

    Link the right css files

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", css_link="/static/css/home.css")
 
 
 @app.route("/stories")
 def stories():
-    return render_template("stories.html")
+    return render_template("stories.html", css_link="/static/css/story-cards.css")
 
 
 @app.route("/puss-in-boots")
 def puss_in_boots():
-    return render_template("puss-in-boots.html")
+    return render_template("puss-in-boots.html", css_link="/static/css/one-story.css")
