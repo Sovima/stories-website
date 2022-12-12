b323e5338356178b67698ac7cd33e32018ada022
Sovima <malashchenko1329@gmail.com>
2022-12-12 17:00:58 -0500

    Update dark header switch

--- static/js/dark-header.js
+++ static/js/dark-header.js
@@ -1,10 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
 	document.querySelector("#light-dark-switch").onclick = function () {
-        if (document.querySelector("#light-dark-switch").classList.contains("light")) {
-            document.querySelector("#header-style").href = "/static/css/dark-header.css"
-        } else {
-            document.querySelector("#header-style").href = "/static/css/header.css"
-        }
-        document.querySelector("#light-dark-switch").classList.toggle("light")
+        document.querySelector("#body").classList.toggle("dark")
 	}
 })
