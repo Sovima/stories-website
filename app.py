e39696f37caf32a074d68376089d68a5d16f7e8d
Sovima <malashchenko1329@gmail.com>
2022-12-14 17:53:35 -0500

    Add mail imports

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_mail import Mail, Message
+import os
 
 app = Flask(__name__)
+# Configure mail settings
+app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+
+
+mail = Mail(app)
+
+
 
 @app.route("/")
 def index():
