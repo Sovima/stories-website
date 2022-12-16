eb0353fc8c9ca5e29c332c63a31466e3ba7feaed
Sovima <malashchenko1329@gmail.com>
2022-12-15 22:19:23 -0500

    configure the gmail password

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request
 from flask_mail import Mail, Message
+from flask_sessions import Session
 import os
 
 app = Flask(__name__)
 # Configure mail settings
-app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
+# app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
@@ -29,3 +30,12 @@ def stories():
 @app.route("/puss-in-boots")
 def puss_in_boots():
     return render_template("puss-in-boots.html", css_link="/static/css/one-story.css")
+
+
+@app.route("/", methods=["POST"])
+def submit_form():
+    user_email = request.form.get("email")
+    message = Message("This is the test email sent from %s" % user_email, sender= os.getenv("MAIL_DEFAULT_SENDER"), 
+                      recipients=["sophiamalashenko@gmail.com"])
+    mail.send(message)
+    return render_template("index.html", css_link = "/static/css/home.css")
