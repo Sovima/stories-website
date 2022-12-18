b6eff12a051da965c4ec2537b28a8fe6515e0625
Sovima <malashchenko1329@gmail.com>
2022-12-18 16:28:22 -0500

    keep dark/light mode

--- static/js/dark-header.js
+++ static/js/dark-header.js
@@ -1,5 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
 	document.querySelector("#light-dark-switch").onclick = function () {
         document.querySelector("#body").classList.toggle("dark")
+		let mode = "light"
+		if (document.querySelector("#body").getAttribute("class") == "dark") {
+			mode = "dark"
+		}
+		fetch("/change-mode", {
+			method : "POST",
+			headers: 
+			{ 
+				'Content-Type': 'application/json'
+			}, 
+			body: JSON.stringify({ mode: mode })
+		})
 	}
+
 })
