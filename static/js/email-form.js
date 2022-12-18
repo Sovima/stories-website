46369990702a7ffb3fe5106d960a20bdfa602f89
Sovima <malashchenko1329@gmail.com>
2022-12-18 18:08:40 -0500

    now sends email without reloading

---
+++ static/js/email-form.js
@@ -0,0 +1,28 @@
+form = document.getElementById("contact-form")
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    firstname = document.getElementById("firstname").value;
+    lastname = document.getElementById("lastname").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    console.log("This is the message: " + message)
+    fetch("/submit-form", {
+        method : "POST",
+        headers: { 
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify({ email: email, 
+                            firstname: firstname, 
+                            lastname: lastname, 
+                            message: message })
+    })
+
+})
+    // fetch("/change-mode", {
+    //     method : "POST",
+    //     headers: 
+    //     { 
+    //         'Content-Type': 'application/json'
+    //     }, 
+    //     body: JSON.stringify({ email: mode })
+    // })
