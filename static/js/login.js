59a2bc9187c216d747ee35b6f4cbf2bc712ba7aa
Sovima <malashchenko1329@gmail.com>
2023-02-03 18:54:20 -0500

    Add submit to login form switch

--- static/js/login.js
+++ static/js/login.js
@@ -32,4 +32,19 @@ function showLogin() {
 function removeLogin() {
     loginButton = document.getElementById("login-window")
     loginButton.classList.remove("showing-login")
+    switchToLogin()
+}
+
+function switchToSignup() {
+    loginFields = document.getElementById("login-form-full")
+    loginFields.classList.add("hide")
+    signupFields = document.getElementById("signup-form-full")
+    signupFields.classList.remove("hide")
+}
+
+function switchToLogin() {
+    loginFields = document.getElementById("signup-form-full")
+    loginFields.classList.add("hide")
+    signupFields = document.getElementById("login-form-full")
+    signupFields.classList.remove("hide")
 }
