ca416a5a8a5331ae73f2e183778e4b2cbcbd47e3
Sovima <malashchenko1329@gmail.com>
2023-11-01 14:29:40 +0000

    Add student/teacher functionality to signup form

--- static/js/signup-form.js
+++ static/js/signup-form.js
@@ -1,8 +1,17 @@
 form = document.getElementById("signup-form")
 form.addEventListener("submit", async function(event) {
+    console.log("sumbitting the signup form");
     event.preventDefault();
     email = document.getElementById("signup-email").value;
     password = document.getElementById("signup-password").value;
+    fistName = document.getElementById("firstName").value;
+    lastName = document.getElementById("lastName").value;
+    userTypes = document.getElementsByName("userType");
+    userType = "Other";
+    for (i = 0; i < userTypes.length; i++) {
+        if (userTypes[i].checked)
+                userType = userTypes[i].value;
+    }
     document.getElementById("signup-form").reset()
     const response = await fetch("/signed-up", {
         method : "POST",
@@ -13,7 +22,8 @@ form.addEventListener("submit", async function(event) {
                             password: password})
     })
     const status = await response.json()
-    console.log(status)
+    console.log(userType);
+    console.log(status);
     console.log(status['status'])
     if ( status['status'] == 'OK' ) {
         location.reload()
